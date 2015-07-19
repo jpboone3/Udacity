912b2208166e34ea413f45749a5c08ed442c48cf
John Boone <jpboone3@gmail.com>
2015-07-19 16:53:30 -0700

    Fix duration calculation and phone UI

---
+++ AndroidStudioProjects/Spotify/app/src/main/java/com/example/administrator/spotify/StreamerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.administrator.spotify;
+
+/**
+ * Created by administrator on 6/26/15.
+ */
+public class StreamerAdapter {
+}
---
+++ AndroidStudioProjects/Spotify/app/src/main/java/com/example/administrator/spotify/StreamerArtist.java
@@ -0,0 +1,10 @@
+package com.example.administrator.spotify;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by administrator on 6/26/15.
+ */
+public class StreamerArtist extends com.spotify.webapi.android.models.Artist {
+    public Bitmap bm;
+}
